62d51a8f4f33c9bdc0f5f6bbd530699a2f93a960
Lisonnnnn <sabbirhasan22k@gmail.com>
2025-12-30 21:16:34 +0600

    shorten function call for component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 function App(){
   return(
      <div>
      <Header />
-    <Note />
+     {
+      notes.map(note =>{
+        return (       
+          <Note  key={note.id} 
+          title={note.title}
+           content={note.content}       
+          />
+        );
+      })
+     }
+    
     <Footer />
     
    </div>
