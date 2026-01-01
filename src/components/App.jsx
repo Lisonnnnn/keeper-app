98ba27958b4193d722f2dd5696420c423b2cead8
Lisonnnnn <sabbirhasan22k@gmail.com>
2026-01-01 12:31:03 +0600

    component based functionality add for Note and createArea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,49 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 function App(){
+  const [notes,setNotes]=React.useState([]);
+  function addNote(Newnote){
+    setNotes((prevNotes)=>{
+      return[...prevNotes,Newnote]
+    });
+    
+  }
+
+  function deleteNote(id){
+    setNotes(PrevNotes=>{
+      return PrevNotes.filter((note,index)=>{
+        return index!==id;
+      });
+    })
+  }
+
+
+
+
+  
   return(
      <div>
      <Header />
+     < CreateArea onAdd={addNote}/>
      {
-      notes.map(note =>{
-        return (       
-          <Note  key={note.id} 
+      notes.map((note,index)=>{
+        return(
+
+          <Note 
+          key={index}
+          id={index}
           title={note.title}
-           content={note.content}       
+          content={note.content}
+          onDelete={deleteNote}
+
+
           />
+
         );
       })
      }
-    
     <Footer />
     
    </div>
