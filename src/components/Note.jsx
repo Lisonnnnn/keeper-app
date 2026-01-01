cb4cfd9d203db5ae0965e2975747e5e72d2484a8
Lisonnnnn <sabbirhasan22k@gmail.com>
2026-01-01 12:31:44 +0600

    deletion handle

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+
+// function deleteNote(id){
+//     return (
+//       notes.filter((note,index)=>index!==id)
+//     );
+//   }
 function Note(props){
+
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={()=>{
+                props.onDelete(props.id);
+            }} >Delete</button>
 
         </div>
     );
